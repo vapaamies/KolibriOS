d7a6530b18c8206c67932cd951472082ca9823c4
Freeman <vapaamies@yandex.ru>
2021-01-10 03:13:15 +0300

    Console Board program KoW support, improvement and bugfix

--- Examples/Console/ConBoard/ConBoard.dpr
+++ Examples/Console/ConBoard/ConBoard.dpr
@@ -10,11 +10,13 @@ var
   Prefix: array[0..3] of KolibriChar; // #0#0#0#0 by program start
   PrefixIndex: LongWord = 0; // 0 by program start but warning
   IsStartLine: Boolean = True;
+{$IFDEF KolibriOS}
   Attr: TFileAttributes;
   BytesWritten: LongWord;
+{$ENDIF}
 
 begin
-  InitConsole('Console board');
+  InitConsole('Console Board');
 
   if CmdLine^ <> #0 then
     LogFilePath := CmdLine;
@@ -37,11 +39,11 @@ begin
             else
               TextColor(LightGray);
           IsStartLine := False;
-          PrefixIndex := Low(Prefix);
           with GetSystemTime do
             con_printf('[%02x:%02x:%02x] ', Hours, Minutes, Seconds);
-          con_write_asciiz(Prefix);
+          con_write_string(Prefix, PrefixIndex + 1);
           LongWord(Prefix) := 0; // Prefix := #0#0#0#0;
+          PrefixIndex := Low(Prefix);
         end
         else
           Inc(PrefixIndex);
@@ -56,12 +58,16 @@ begin
         end;
       end;
 
+    {$IFDEF KolibriOS}
       if GetFileAttributes(LogFilePath, Attr) = ERROR_FILE_NOT_FOUND then
       begin
         CreateFile(LogFilePath);
         Attr.Size := 0;
       end;
       WriteFile(LogFilePath, Ch, SizeOf(Ch), Attr.Size, BytesWritten);
-    end;
+    {$ENDIF}
+    end
+    else
+      Sleep(1);
   until LongBool(con_get_flags and CON_WINDOW_CLOSED);
 end.
